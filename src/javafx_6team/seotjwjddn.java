889f57fefff98a9a8c2702da323362c0298b661c
seotj <seotj@jungwoo>
2022-08-23 09:38:14 +0900

    패키지

--- src/javafx_6team/seotjwjddn.java
+++
@@ -1,8 +0,0 @@
-package javafx_6team;
-
-public class seotjwjddn {
-	public static void main(String[] args) {
-		System.out.println("서정우");
-	}
-
-}
---
+++ src/jeoungwoo/jeoungwoo.java
@@ -0,0 +1,5 @@
+package jeoungwoo;
+
+public class jeoungwoo {
+
+}
